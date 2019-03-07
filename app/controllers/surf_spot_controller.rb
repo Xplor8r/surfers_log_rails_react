e811b68a2214f0533946928ad9910ecf78b291de
Xplor8r <mpieschel39@gmail.com>
2019-03-06 16:35:31 -0800

    generate SurfSpot Controller

---
+++ app/controllers/surf_spot_controller.rb
@@ -0,0 +1,2 @@
+class SurfSpotController < ApplicationController
+end
---
+++ test/controllers/surf_spot_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SurfSpotControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
