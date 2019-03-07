14f25c6b20c10e0fa4700fb6db87a6d1f3932090
Xplor8r <mpieschel39@gmail.com>
2019-03-06 18:08:38 -0800

    create table for users log entries comments install gems

--- db/migrate/20190307003817_create_users.rb
+++ db/migrate/20190307003817_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :slug, null: false
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin, default: false
+      t.string :uid
       t.timestamps
     end
   end
--- db/migrate/20190307004107_create_log_entries.rb
+++ db/migrate/20190307004107_create_log_entries.rb
@@ -1,7 +1,12 @@
 class CreateLogEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :log_entries do |t|
-
+      t.integer :brewery_state_id
+      t.integer :user_id
+      t.string :surf_spot, null: false
+      t.string :slug, null: false
+      t.integer :comments_count, default: 0
+      
       t.timestamps
     end
   end
