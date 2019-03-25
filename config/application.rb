656bc85d5f36f847c606500ba9660905dd0ff55a
Xplor8r <mpieschel39@gmail.com>
2019-03-25 16:17:12 -0700

    install jwt and rack-cors gems set up for middleware

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module SurfersLogRailsReact
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+    config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
