f0f06bf4e7aa05d7e05d27a28725b776c89003cf
Xplor8r <mpieschel39@gmail.com>
2019-03-29 18:41:59 -0700

    set up sessions and users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+  
+    def create
+      @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          flash[:error] = "Sorry, that email and password doesn't match our records. Please sign up."
+          redirect_to new_user_url
+        end
+    end
+    
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_path
+    end    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+    before_action :user, only: [:show, :states]
+
+    def show
+        @log_entries = LogEntry.where(user: @user).sorted.includes(:user, :country, :surf_spot)
+        respond_to do |format|
+            format.html {render :show}
+            format.json {render json: @user}
+        end
+    end
+  
+    def states
+    end
+  
+    def index
+        render "log_entries/index"
+    end
+  
+    def new
+        @user = User.new
+    end
+  
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "You must give a valid name, email and password."
+            redirect_to new_user_path
+        end
+    end
+  
+    private
+        def user
+            @user = User.friendly.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            flash[:error] = "Sorry, something went wrong."
+            redirect_to root_path 
+        end
+    
+        def user_params
+            params.require(:user).permit(:name, :admin, :password, :email)
+        end
 end
