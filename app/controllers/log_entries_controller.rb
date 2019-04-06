f5886473a43e4321d04e4568f2540af6b3baf424
Xplor8r <mpieschel39@gmail.com>
2019-04-05 20:13:24 -0700

    more work on seeds file log_entries controller render json only no slug for logEntry

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -6,20 +6,14 @@ class LogEntriesController < ApplicationController
   
     def index
       @log_entries = LogEntry.sorted.includes(:user, :country, :surf_spot)
-      respond_to do |format|
-        format.html {render :index}
-        format.json {render json: @log_entries}
-      end
+      render json: @log_entries
     end
   
     def show
       @log_entries = LogEntry.all
       @post = Post.new
       @post.user = current_user
-      respond_to do |format|
-        format.html {render :show}
-        format.json {render json: @log_entries}
-      end
+      render json: @log_entries
     end
   
     def new
@@ -31,10 +25,7 @@ class LogEntriesController < ApplicationController
       @log_entries = current_user.log_entries.new(log_entry_params)
       @log_entries.posts.each { |post| post.user_id = current_user.id }
       if @log_entries.save
-        respond_to do |format|
-          format.html {redirect_to log_entry_path(@log_entries)}
-          format.json {render json: @log_entries}
-        end
+      render json: @log_entries
       else
         render json: @log_entries, status: 406
       end
--- db/migrate/20190307004107_create_log_entries.rb
+++ db/migrate/20190307004107_create_log_entries.rb
@@ -7,12 +7,9 @@ class CreateLogEntries < ActiveRecord::Migration[5.2]
       t.string :conditions
       t.string :wave_count
       t.string :image_url
-
       t.integer :country_id, null:false
       t.integer :user_id
       t.integer :surf_spot_id, null: false
-      t.string :slug, null: false
-
       t.integer :posts_count, default: 0
       
       t.timestamps
