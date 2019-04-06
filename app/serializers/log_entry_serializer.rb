0b63e8636660dfbbb6d821d4a0d70b0773e73ecd
Xplor8r <mpieschel39@gmail.com>
2019-04-05 20:58:09 -0700

    update logEntry serializer

--- app/serializers/log_entry_serializer.rb
+++ app/serializers/log_entry_serializer.rb
@@ -1,5 +1,13 @@
 class LogEntrySerializer < ActiveModel::Serializer
-  attributes :id, :surf_spot_id, :country_id, :created_at
+  attributes :id,
+    :date,
+    :swell_direction,
+    :swell_size,
+    :conditions,
+    :wave_count,
+    :image_url,
+    :country_id,
+    :surf_spot_id
   has_many :posts
   belongs_to :country
   belongs_to :surf_spot
