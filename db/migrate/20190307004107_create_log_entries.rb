3f957f46a443a72d0f09fb30fb599dcd1aba335c
Xplor8r <mpieschel39@gmail.com>
2019-06-20 20:25:56 -0700

    3 swell direction and size conditions to wind speed and direction add time to log entries table

--- db/migrate/20190307004107_create_log_entries.rb
+++ db/migrate/20190307004107_create_log_entries.rb
@@ -2,10 +2,16 @@ class CreateLogEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :log_entries do |t|
       t.integer :date
-      t.string :swell_direction
-      t.string :swell_size
-      t.string :conditions
-      t.string :wave_count
+      t.integer :time
+      t.string :swell_1_direction
+      t.integer :swell_1_size
+      t.string :swell_2_direction
+      t.integer :swell_2_size
+      t.string :swell_3_direction
+      t.integer :swell_3_size
+      t.integer :wind_speed
+      t.string :wind_direction
+      t.integer :wave_count
       t.string :image_url
       t.integer :country_id, null:false
       t.integer :user_id
