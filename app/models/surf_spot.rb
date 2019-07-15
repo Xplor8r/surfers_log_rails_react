ce46eb6aaa959d56f021c63625f58fc0468918cf
Xplor8r <mpieschel39@gmail.com>
2019-07-15 15:16:12 -0700

    add surf spots and countries to user serializer surf spot has many log entries through country

--- app/models/surf_spot.rb
+++ app/models/surf_spot.rb
@@ -2,7 +2,7 @@ class SurfSpot < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
     belongs_to :country
-    has_many :log_entries, through: :log_entries
+    has_many :log_entries, through: :country
     has_many :users, through: :log_entries
     
     scope :sorted, ->{order(name: :asc)}
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,6 @@ class UserSerializer < ActiveModel::Serializer
   has_many :friendships
   has_many :log_entries
   has_many :posts
+  has_many :surf_spots
+  has_many :countries
 end
