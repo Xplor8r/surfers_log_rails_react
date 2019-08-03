788b348bc42a61c6b984fa3bfdb316860e2fc3eb
Xplor8r <mpieschel39@gmail.com>
2019-08-03 15:42:02 -0700

    npm install set proxy to local host 3001 call api to render log entries

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+const LogEntry = ({logEntry}) => {
+  return (
+    <li>
+      <h1>{logEntry.surf_spot.name}</h1>
+      <h2>{logEntry.country.name}</h2>
+      <h3>By {logEntry.user.name}</h3>
+      <p>{logEntry.posts[0].content}</p>
+      <p>Comments: {logEntry.posts.length - 1}</p>
+      <p>Rating: {logEntry.rating}</p>
+      <p>Date: {logEntry.time} {logEntry.date}</p>
+      <img src={logEntry.image_url}/>
+      <p>Swell: {logEntry.swell_1_direction} {logEntry.swell_1_size}</p>
+      <p>Wave Count: {logEntry.wave_count}</p>
+      <p>Wind: {logEntry.wind_direction} {logEntry.wind_speed}</p>
+    </li>
+  )
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        logEntries: [],
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+  fetchData() {
+    return (
+      fetch('/RailsApi/log_entries', { method: 'GET' })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ logEntries: data })
+      })
+    )
+  }
+  componentWillMount(){
+    this.fetchData();
+  }
   render() {
+    let logEntries = this.state.logEntries
+    console.log(logEntries)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <ul>
+          {logEntries.map((logEntry) => (
+            <LogEntry key={logEntry.id} logEntry={logEntry} />
+          ))}
+        </ul>
         </header>
       </div>
     );
