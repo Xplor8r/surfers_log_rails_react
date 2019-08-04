b733298f1f16a28795bbb0dac2ba00878e80e2e5
Xplor8r <mpieschel39@gmail.com>
2019-08-03 22:39:00 -0700

    install react star rating component improve time and date reorder rendered data

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 import Moment from 'react-moment'
 import 'moment-timezone';
 import Truncate from 'react-truncate';
+import StarRatingComponent from 'react-star-rating-component';
 
 const LogEntry = ({logEntry}) => {
   return (
-    <li>
-      <h1>{logEntry.surf_spot.name}, {logEntry.country.name}</h1>
+    <div>
+      <h1>{logEntry.surf_spot.name} {logEntry.country.name}</h1>
       <h2>By {logEntry.user.name}</h2>
-      {logEntry.date && <p><Moment parse="YYYYMMDD HH:mm">{logEntry.date} {logEntry.time}</Moment></p>}
-      <p><Truncate lines={2} width={600}>{logEntry.posts[0].content}</Truncate></p>
-      <p>Comments: {logEntry.posts.length - 1}</p>
-      {logEntry.rating  && <p>Rating: {logEntry.rating}</p>}
-      <img width={600}src={logEntry.image_url}/>
+      {logEntry.date && logEntry.time ?
+        <p><Moment parse="YYYYMMDD HH:mm" format="MM/DD/YYYY HH:mm a">{logEntry.date} {logEntry.time}</Moment></p>:
+        <p><Moment parse="YYYYMMDD" format="MM/DD/YYYY">{logEntry.date}</Moment></p>
+      }
+      {logEntry.rating  && <StarRatingComponent starCount={5} value={logEntry.rating}/>}
       { logEntry.swell_1_size && logEntry.swell_1_direction && <p>Swell: {logEntry.swell_1_size} ft @ {logEntry.swell_1_direction} sec</p>}
       {logEntry.wave_count && <p>Wave Count: {logEntry.wave_count}</p>}
       {logEntry.wind_direction && <p>Wind: {logEntry.wind_direction} @ {logEntry.wind_speed} mph </p>}
-    </li>
+      <p><Truncate lines={2} width={600}>{logEntry.posts[0].content}</Truncate></p>
+      <p>Comments: {logEntry.posts.length - 1}</p>
+      {logEntry.image_url && <img width={600} src={logEntry.image_url} alt={"Surfing at " + logEntry.surf_spot.name}/>}
+    </div>
   )
 }
 
@@ -44,18 +48,17 @@ class App extends Component {
   }
   render() {
     let logEntries = this.state.logEntries
-    console.log(logEntries)
     return (
       <div className="App">
         <header className="App-header">
         <div>
           {logEntries.length === 0 ?
           <img src={logo} className="App-logo" alt="logo" /> :
-          <ul>
+          <div>
             {logEntries.map((logEntry) => (
               <LogEntry key={logEntry.id} logEntry={logEntry} />
             ))}
-          </ul>}
+          </div>}
         </div>
         </header>
       </div>
