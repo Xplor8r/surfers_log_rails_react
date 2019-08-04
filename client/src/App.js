4b2cd174b494ba0db7a58f35de9e588e86388051
Xplor8r <mpieschel39@gmail.com>
2019-08-04 16:40:35 -0700

    remove redux-filter add name to star rating comments if > 1 fix surf spot error in seed file

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,12 @@ const LogEntry = ({logEntry}) => {
         <p><Moment parse="YYYYMMDD HH:mm" format="MM/DD/YYYY HH:mm a">{logEntry.date} {logEntry.time}</Moment></p>:
         <p><Moment parse="YYYYMMDD" format="MM/DD/YYYY">{logEntry.date}</Moment></p>
       }
-      {logEntry.rating  && <StarRatingComponent starCount={5} value={logEntry.rating}/>}
+      {logEntry.rating  && <StarRatingComponent name={'rating'} starCount={5} value={logEntry.rating}/>}
       { logEntry.swell_1_size && logEntry.swell_1_direction && <p>Swell: {logEntry.swell_1_size} ft {logEntry.swell_1_direction}</p>}
       {logEntry.wave_count && <p>Wave Count: {logEntry.wave_count}</p>}
       {logEntry.wind_direction && <p>Wind: {logEntry.wind_direction} @ {logEntry.wind_speed} mph </p>}
       <p><Truncate lines={2} width={600}>{logEntry.posts[0].content}</Truncate></p>
-      <p>Comments: {logEntry.posts.length - 1}</p>
+      {logEntry.posts.length > 1 && <p>Comments: {logEntry.posts.length - 1}</p>}
       {logEntry.image_url && <img width={600} src={logEntry.image_url} alt={"Surfing at " + logEntry.surf_spot.name}/>}
     </div>
   )
