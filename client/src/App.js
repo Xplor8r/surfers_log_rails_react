ed75db12e853a54e0b020300c52e061bd3b54c27
Xplor8r <mpieschel39@gmail.com>
2019-08-03 17:12:34 -0700

    install react-truncate for shortening rendered log entry content

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Moment from 'react-moment'
 import 'moment-timezone';
+import Truncate from 'react-truncate';
 
 const LogEntry = ({logEntry}) => {
   return (
@@ -10,10 +11,10 @@ const LogEntry = ({logEntry}) => {
       <h1>{logEntry.surf_spot.name}, {logEntry.country.name}</h1>
       <h2>By {logEntry.user.name}</h2>
       {logEntry.date && <p><Moment parse="YYYYMMDD HH:mm">{logEntry.date} {logEntry.time}</Moment></p>}
-      <p>{logEntry.posts[0].content}</p>
+      <p><Truncate lines={2} width={600}>{logEntry.posts[0].content}</Truncate></p>
       <p>Comments: {logEntry.posts.length - 1}</p>
       {logEntry.rating  && <p>Rating: {logEntry.rating}</p>}
-      <img src={logEntry.image_url}/>
+      <img width={600}src={logEntry.image_url}/>
       { logEntry.swell_1_size && logEntry.swell_1_direction && <p>Swell: {logEntry.swell_1_size} ft @ {logEntry.swell_1_direction} sec</p>}
       {logEntry.wave_count && <p>Wave Count: {logEntry.wave_count}</p>}
       {logEntry.wind_direction && <p>Wind: {logEntry.wind_direction} @ {logEntry.wind_speed} mph </p>}
