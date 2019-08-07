20b64422d9210153916b927c6f3aa8f92d116bec
Xplor8r <mpieschel39@gmail.com>
2019-08-07 16:48:53 -0700

    add listgroup with listgroupitems to sidebar

--- client/src/Components/sidebar.js
+++ client/src/Components/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardTitle, Col }  from 'reactstrap';
+import { Card, CardTitle, Col, ListGroup, ListGroupItem, Badge }  from 'reactstrap';
 
 const SideBar = () => {
     return (
@@ -7,6 +7,26 @@ const SideBar = () => {
             <Card style={{backgroundColor: 'grey', margin: '.5rem', height: '100%'}}>
                 <CardTitle>
                     <h1>Side Bar</h1>
+                    <ListGroup>
+                        <ListGroupItem 
+                            style={{backgroundColor: 'grey'}}
+                            className="justify-content-between"
+                        >
+                            USA <Badge pill>13</Badge>
+                        </ListGroupItem>
+                        <ListGroupItem 
+                            style={{backgroundColor: 'grey'}}
+                            className="justify-content-between"
+                        >
+                            Australia <Badge pill>2</Badge>
+                        </ListGroupItem>
+                        <ListGroupItem
+                            style={{backgroundColor: 'grey'}}
+                            className="justify-content-between"
+                        >
+                            Tahiti <Badge pill>1</Badge>
+                        </ListGroupItem>
+                    </ListGroup>
                 </CardTitle>
             </Card>
         </Col>
