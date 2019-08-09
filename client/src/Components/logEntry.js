183d178352e089dba6bde1945908a87281cbe9dd
Xplor8r <mpieschel39@gmail.com>
2019-08-08 19:57:35 -0700

    fix errors and html format in log Entry Component

--- client/src/Components/logEntry.js
+++ client/src/Components/logEntry.js
@@ -20,59 +20,49 @@ const LogEntry = ({logEntry}) => {
         </CardSubtitle>
         <CardText>
           {logEntry.date && logEntry.time ?
-            <p>
               <Moment
                 parse="YYYYMMDD HH:mm"
                 format="MM/DD/YYYY HH:mm a"
               >
               {logEntry.date} {logEntry.time}
-              </Moment>
-            </p>:
-            <p>
+              </Moment>:
               <Moment
                 parse="YYYYMMDD"
                 format="MM/DD/YYYY"
               >
               {logEntry.date}
               </Moment>
-            </p>
           }
         </CardText>
-        <CardText>
-          {logEntry.rating ?
+          {logEntry.rating &&
             <StarRatingComponent
               name={'rating'}
               starCount={5}
               value={logEntry.rating}
-            />:
-            <p>No Rating</p>
-          }
-        </CardText>
+            />}
         <CardText>
-          { logEntry.swell_1_size &&
-            logEntry.swell_1_direction &&
-            <p>Swell: {logEntry.swell_1_size} ft {logEntry.swell_1_direction}</p>
-          }
+            Swell: {logEntry.swell_1_size} ft {logEntry.swell_1_direction}
         </CardText>
+        {logEntry.wave_count && <CardText>
+          Wave Count: {logEntry.wave_count}
+        </CardText>}
+        {logEntry.wind_direction && <CardText>
+            Wind: {logEntry.wind_direction} @ {logEntry.wind_speed} mph
+        </CardText>}
         <CardText>
-          {logEntry.wave_count && <p>Wave Count: {logEntry.wave_count}</p>}
-        </CardText>
-        <CardText>
-          {logEntry.wind_direction &&
-            <p>Wind: {logEntry.wind_direction} @ {logEntry.wind_speed} mph </p>
-          }
-        </CardText>
-        <CardText>
-          <p>
             <Truncate lines={2} width={600}>
               {logEntry.posts[0].content}
             </Truncate>
-          </p>
         </CardText>
-        <CardText>
-        {logEntry.posts.length > 1 && <p>Comments: {logEntry.posts.length - 1}</p>}
-        </CardText>
-        {logEntry.image_url && <CardImg bottom width='50%' src={logEntry.image_url} alt={"Surfing at " + logEntry.surf_spot.name}/>}
+        {logEntry.posts.length > 1 && <CardText>
+          Comments: {logEntry.posts.length - 1}
+        </CardText>}
+        {logEntry.image_url && <CardImg
+          bottom
+          width='50%'
+          src={logEntry.image_url}
+          alt={"Surfing at " + logEntry.surf_spot.name}
+        />}
         </CardBody>
       </Card>
     )
