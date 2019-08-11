e746a3fd50b3e97a41424e3df9411d4eb1b9847a
Xplor8r <mpieschel39@gmail.com>
2019-08-10 22:38:08 -0700

    extract and import country to sidebar filter alpha with no log entries

--- client/src/Components/sidebar.js
+++ client/src/Components/sidebar.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Truncate from 'react-truncate';
-import { Card, CardText, Col, ListGroup, ListGroupItem, Badge, Nav,
-    UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem }  from 'reactstrap';
+import { Card, CardText, Col, ListGroup, ListGroupItem }  from 'reactstrap';
+import Country from './country';
 
 const SideBar = ({countries}) => {
     let topList = countries.filter(a=>a.log_entries.length > 0);
-    let alphaList = countries.sort((a,b)=> a.name.localeCompare(b.name));
+    let alphaList = countries.sort((a,b)=> a.name.localeCompare(b.name)).filter(a=>a.log_entries.length === 0);
 
     return (
         <Col xs="3" style={{ padding: '0px'}}>
@@ -18,25 +17,7 @@ const SideBar = ({countries}) => {
                                 key={country.id}
                                 className="justify-content-center"
                             >
-                                <Nav>
-                                <UncontrolledDropdown nav inNavbar>
-                                        <DropdownToggle className="coral" nav caret>
-                                            {country.name} <Badge pill>{country.log_entries.length} Log Entries</Badge>                          
-                                        </DropdownToggle>
-                                        <DropdownMenu left="true">
-                                            <DropdownItem header>Surf Spots</DropdownItem>
-                                            <DropdownItem divider />
-                                            {country.surf_spots.map((spot)=> (
-                                                <DropdownItem key={spot.id}>
-                                                    <Truncate lines={1} width={150}>
-                                                        {spot.name}
-                                                    </Truncate>
-                                                </DropdownItem>
-                                            ))}
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>
-                                </Nav>
-        
+                                <Country country={country}/>
                             </ListGroupItem>
                         ))}
                         {alphaList.map((country) => (
@@ -44,21 +25,7 @@ const SideBar = ({countries}) => {
                                 key={country.id}
                                 className="justify-content-center"
                             >     
-                                <Nav>
-                                    <UncontrolledDropdown nav inNavbar>
-                                        <DropdownToggle className="coral" nav caret>
-                                            {country.name}
-                                        </DropdownToggle>
-                                        <DropdownMenu left="true">
-                                            <DropdownItem header>No Log Entries</DropdownItem>
-                                            <DropdownItem divider />
-                                            <DropdownItem>
-                                                Create a log Entry
-                                            </DropdownItem>
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>
-                                </Nav>
-
+                                <Country country={country}/>
                             </ListGroupItem>
                         ))}
                     </ListGroup>
