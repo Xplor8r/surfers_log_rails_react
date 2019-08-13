98a2d65b88dc36e11815d02e08fbdd6124cede2d
Xplor8r <mpieschel39@gmail.com>
2019-08-12 17:20:08 -0700

    create surf spots action to fetch surf spot data

---
+++ client/src/Actions/surfSpots.js
@@ -0,0 +1,19 @@
+import { endDataFetch } from './dataFetch'
+
+export const fetchSurfSpotData = () => {
+    return dispatch => {
+        fetch('/RailsApi/log_entries/surf_spots', { method: 'GET' })
+        .then(response => response.json())
+        .then(surfSpotData => {
+            dispatch(getSurfSpots(surfSpotData))
+            dispatch(endDataFetch())
+        })
+    }    
+}
+
+const getSurfSpots = surfSpotData => {
+    return {
+        type: 'GET_SURF_SPOT',
+        surfSpotData
+    };
+}
