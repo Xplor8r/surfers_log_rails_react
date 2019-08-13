8705b411ce1f76fd9e7f0c38e59b12e2b8c6cd5b
Xplor8r <mpieschel39@gmail.com>
2019-08-13 15:40:09 -0700

    remove conditionals in country component

--- client/src/Components/country.js
+++ client/src/Components/country.js
@@ -1,36 +1,28 @@
 import React from 'react';
-import { Badge, Nav, UncontrolledDropdown, DropdownToggle,
+import { Badge, UncontrolledDropdown, DropdownToggle,
     DropdownMenu, DropdownItem } from 'reactstrap';
 import Truncate from 'react-truncate';
 
 const Country = ({country}) => {
     return (
-        <Nav>
             <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle className="coral" nav caret>
                     {country.name} {' '}
-                    {country.log_entries.length > 0 &&
-                        <Badge pill>{country.log_entries.length}</Badge>}             
+                    <Badge pill>{country.log_entries.length}</Badge>          
                 </DropdownToggle>
                 <DropdownMenu left="true">
-                    {country.log_entries.length > 0 ? 
-                        <DropdownItem header>Surf Spots</DropdownItem>:
-                        <DropdownItem header>No Log Entries</DropdownItem>
-                    }              
+                    <DropdownItem>Log Entries</DropdownItem>
                     <DropdownItem divider />
-                    {country.log_entries.length > 0 ? 
-                    country.surf_spots.map((spot)=> (
+                    <DropdownItem header>Surf Spots</DropdownItem>       
+                    {country.surf_spots.map((spot)=> (
                         <DropdownItem key={spot.id}>
                             <Truncate lines={1} width={150}>
                                 {spot.name}
                             </Truncate>
                         </DropdownItem>
-                    )):
-                        <DropdownItem>Create a log Entry</DropdownItem>
-                    }
+                    ))}
                 </DropdownMenu>
             </UncontrolledDropdown>
-        </Nav>
     )
 }
 
