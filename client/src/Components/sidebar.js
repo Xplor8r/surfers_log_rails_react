2806b62bb12cb8aae506f9d12ccbfc01c9487d34
Xplor8r <mpieschel39@gmail.com>
2019-08-12 17:21:25 -0700

    set up sidebar to render list of surf spots

--- client/src/Components/sidebar.js
+++ client/src/Components/sidebar.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Card, CardText, Col, ListGroup, ListGroupItem }  from 'reactstrap';
+import { Card, CardTitle, CardText, Col, ListGroup, ListGroupItem }  from 'reactstrap';
 import Country from './country';
 
-const SideBar = ({countries}) => {
+const SideBar = ({countries, surfSpots}) => {
     let topList = countries.filter(a=>a.log_entries.length > 0);
     let alphaList = countries.sort((a,b)=> a.name.localeCompare(b.name)).filter(a=>a.log_entries.length === 0);
 
     return (
         <Col xs="3" style={{ padding: '0px'}}>
             <Card style={{ margin: '2rem .5rem 1rem .5rem'}}>
+                <CardTitle><h2>Surf Spots</h2></CardTitle>
                 <CardText>
-                    <h2>Countries</h2>
+                    <ListGroup>
+                        {surfSpots.map((surfSpot) => (
+                            <ListGroupItem 
+                                key={surfSpot.id}
+                                className="justify-content-center"
+                            >
+                                {surfSpot.name}
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+                </CardText>
+            </Card>
+            <Card style={{ margin: '2rem .5rem 1rem .5rem'}}>
+                <CardTitle><h2>Countries</h2></CardTitle>
+                <CardText>
                     <ListGroup>
                         {topList.map((country) => (
                             <ListGroupItem 
