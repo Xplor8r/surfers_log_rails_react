88ad4e9d5735a5f6c0649462c4f6773ce0e12061
Xplor8r <mpieschel39@gmail.com>
2019-08-15 20:00:54 -0700

    create logEntriesByUser container

---
+++ client/src/Containers/logEntriesByUser.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Col } from 'reactstrap';
+import LogEntry from '../Components/logEntry';
+
+class LogEntriesByUser extends Component {
+    componentWillMount(){
+
+    }
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+    render() {
+        let logEntries = this.props.logEntryData;
+        return (
+            <Col xs="9" style={{ padding: '0px'}}>
+                {logEntries.map((logEntry) => (
+                    <LogEntry key={logEntry.id} logEntry={logEntry} />
+                ))}
+            </Col>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        logEntryData: state.logEntryData
+    }
+  }
+  
+export default connect(mapStateToProps)(LogEntriesByUser)
