e03dcd359ffab966bb7a6c01ea50d2e63142c8b6
Xplor8r <mpieschel39@gmail.com>
2019-08-15 22:06:32 -0700

    create fetch actions for log entry data by country by surf spot and by user

--- client/src/Actions/logEntries.js
+++ client/src/Actions/logEntries.js
@@ -1,4 +1,5 @@
 import { endDataFetch } from './dataFetch'
+import { beginDataFetch } from './dataFetch'
 
 export const fetchLogEntryData = () => {
     return dispatch => {
@@ -16,4 +17,39 @@ const getLogEntryData = logEntryData => {
         type: 'GET_LOG_ENTRY_DATA',
         logEntryData
     };
+}
+
+export const fetchLogEntryDataByCountry = (id) => {
+    return dispatch => {
+        fetch('/RailsApi/log_entries/country/' + id, { method: 'GET' })
+        .then(response => response.json())
+        .then(logEntryData => {
+            dispatch(beginDataFetch())
+            dispatch(getLogEntryData(logEntryData))
+            dispatch(endDataFetch())
+        })
+    }    
+}
+
+export const fetchLogEntryDataBySurfSpot = (id) => {
+    return dispatch => {
+        fetch('/RailsApi/log_entries/surf_spot/' + id, { method: 'GET' })
+        .then(response => response.json())
+        .then(logEntryData => {
+            dispatch(getLogEntryData(logEntryData))
+            dispatch(endDataFetch())
+        })
+    }    
+}
+
+export const fetchLogEntryDataByUser = (id) => {
+    return dispatch => {
+        fetch('/RailsApi/users/' + id, { method: 'GET' })
+        .then(response => response.json())
+        .then(userData => {
+            let logEntryData = userData.log_entries
+            dispatch(getLogEntryData(logEntryData))
+            dispatch(endDataFetch())
+        })
+    }    
 }
