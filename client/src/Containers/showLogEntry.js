4cee7e0f787add38b8c98c2501cbe87905042436
Xplor8r <mpieschel39@gmail.com>
2019-08-15 20:01:39 -0700

    create showLogEntry container

---
+++ client/src/Containers/showLogEntry.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Col } from 'reactstrap';
+import LogEntry from '../Components/logEntry';
+
+class LogEntriesByUser extends Component {
+    componentWillMount(){
+
+    }
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+    render() {
+        return (
+            <Col xs="9" style={{ padding: '0px'}}>
+                <LogEntry key={logEntry.id} logEntry={logEntry} />
+            </Col>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        logEntryData: state.logEntryData
+    }
+  }
+  
+export default connect(mapStateToProps)(LogEntriesByUser)
