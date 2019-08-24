244ea0bfc7911ecb3637902235734a6c0ee11c42
Xplor8r <mpieschel39@gmail.com>
2019-08-23 19:01:25 -0700

    clean up NavBarComponent code implement hooks

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -1,70 +1,53 @@
-import React, { Component }from 'react';
+import React, { useState }from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, Media } from 'reactstrap';
 import surfLogo from '../images/surfers_log_logo.jpg'
 import CountryLink from './linkToCountry'
 import SurfSpotLink from './linkToSurfSpot'
 import ModalForm from './modalForm'
 
-class NavBarComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
+const NavBarComponent = ({countries,surfSpots}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  
-  handleClick = (e)=> {
-    e.preventDefault();
-    window.scrollTo(0, 0);
-  }
+  const countriesWithLogEntries = countries.filter(a=>a.log_entries.length > 0);
+  const surfSpotsWithLogEntries = surfSpots.filter(a=>a.log_entries.length > 0);
 
-  render() {
-    let countriesWithLogEntries = this.props.countries.filter(a=>a.log_entries.length > 0);
-    let surfSpotsWithLogEntries = this.props.surfSpots.filter(a=>a.log_entries.length > 0);
-    return (
-        <Navbar sticky="top" style={{backgroundColor: "#7cbcc6"}} light expand="md" className={this.state.scroll > this.state.top ? "fixed-nav": ""}>
-          <NavbarBrand href="/" onClick={(e) => this.handleClick(e)}>
-            <Media src={surfLogo} height={60} alt="Surfers Log Logo" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <ModalForm form={"Log In"}/>
-              <ModalForm form={"Sign Up"}/>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Countries
-                </DropdownToggle>           
-                <DropdownMenu right>
-                  {countriesWithLogEntries.map((country) => (
-                    <CountryLink country={country} key={country.id}/>
-                  ))}
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Surf Spots
-                </DropdownToggle>
-            
-                <DropdownMenu right>
-                  {surfSpotsWithLogEntries.map((surfSpot) => (
-                    <SurfSpotLink surfSpot={surfSpot} key={surfSpot.id}/>
-                  ))}
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-    );
-  }
+  return (
+    <Navbar sticky="top" style={{backgroundColor: "#7cbcc6"}} light expand="md">
+      <NavbarBrand href="/">
+        <Media src={surfLogo} height={60} alt="Surfers Log Logo" />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <ModalForm form={"Log In"}/>
+          <ModalForm form={"Sign Up"}/>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Countries
+            </DropdownToggle>           
+            <DropdownMenu right>
+              {countriesWithLogEntries.map((country) => (
+                <CountryLink country={country} key={country.id}/>
+              ))}
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Surf Spots
+            </DropdownToggle>   
+            <DropdownMenu right>
+              {surfSpotsWithLogEntries.map((surfSpot) => (
+                <SurfSpotLink surfSpot={surfSpot} key={surfSpot.id}/>
+              ))}
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
 }
 
 export default NavBarComponent
