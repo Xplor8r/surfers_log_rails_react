2eb5bc08491ad27d8402bec8a8b4a83ec9b6c941
Xplor8r <mpieschel39@gmail.com>
2019-08-26 20:17:33 -0700

    truncate comments add see more link

--- client/src/Components/comment.js
+++ client/src/Components/comment.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Truncate from 'react-truncate';
 import { CardText } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 const Comment = ({comment}) => {
+    const [lines] = useState(1);
+    const [expanded, setExpanded] = useState(false);
+    const toggleLines = (e) => {
+        e.preventDefault();
+        setExpanded(!expanded);
+    }
     return (
         <CardText>
             <strong>{comment.user.name}</strong> <span>posted:</span><br/>
-            <Truncate lines={1}>{comment.content}</Truncate>
+            <Truncate 
+                lines={!expanded && lines}
+                ellipsis={(
+                <span>... <Link 
+                    className="coral"
+                    to={`/log-entry/${comment.log_entry_id}`}
+                    onClick={(e) => toggleLines(e)}
+                    >
+                        See More
+                    </Link>
+                </span>
+                )}
+            >
+                {comment.content}
+            </Truncate>
         </CardText>
     )
 }
