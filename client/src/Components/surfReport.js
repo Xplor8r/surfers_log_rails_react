8098ee51c0316f5d1aa9c81942e56a535e7586ba
Michael Pieschel <mpieschel39@gmail.com>
2019-08-27 18:31:46 -0700

    use noaa image as stub for surf report card

--- client/src/Components/surfReport.js
+++ client/src/Components/surfReport.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, Col } from 'reactstrap';
 import Moment from 'react-moment';
-import Pic from "../images/mikeysurf1.jpg";
+import Pic from "../images/noaa.jpg";
 
 const SurfReport = () => {
   return (
     <Col xs="3" style={{ padding: '0px'}}>
       <Card style={{position: 'sticky', top: '120px'}}>
-        <CardImg top width="100%" src={Pic} alt="Mike surfing" />
+        <CardImg top width="100%" src={Pic} alt="Noaa swell image" />
         <CardBody style={{textAlign: 'left'}}>
           <CardTitle><strong>Get Local Surf Report</strong></CardTitle>
           <CardSubtitle><Moment local></Moment></CardSubtitle>
