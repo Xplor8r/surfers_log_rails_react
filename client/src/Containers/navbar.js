10f1352a074df9137a5e13bb6eeba1f684784333
Xplor8r <mpieschel39@gmail.com>
2019-09-10 21:47:19 -0700

    pass navToggle prop to ModalForms and NavBarItems if isMobile is true

--- client/src/Containers/navbar.js
+++ client/src/Containers/navbar.js
@@ -5,36 +5,51 @@ import NavBarItem from '../Components/navBarItem'
 import ModalForm from '../Components/modalForm'
 import  { Link } from 'react-router-dom';
 
-const NavBarComponent = ({countries, surfSpots, surfers}) => {
+const NavBarComponent = ({countries, surfSpots, surfers, isMobile}) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-
+  
   const countriesWithLogEntries = countries.filter(a=>a.log_entries.length > 0);
   const surfSpotsWithLogEntries = surfSpots.filter(a=>a.log_entries.length > 0);
 
   return (
-    <Navbar sticky="top" style={{top: '-1px', backgroundColor: "#7cbcc6"}} light expand="md">
+    <Navbar
+      light
+      expand="md"
+      sticky="top"
+      style={{top: '-1px', backgroundColor: "#7cbcc6"}}
+    >
       <Link
         className="navbar-brand"
         to={{
           pathname:'/',
-          state: {
-            type: 'all'
-          }
+          state: {type: 'all'}
         }}
-        onClick={()=>window.scrollTo(0, 0)}
+        onClick={isOpen && isMobile ? toggle: undefined}
       >
         <Media src={surfLogo} className="logo" alt="Surfers Log Logo" />
       </Link>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
-          <ModalForm form={"Log In"}/>
-          <ModalForm form={"Sign Up"}/>
+          <ModalForm form={"Log In"} navToggle={isMobile ? toggle: undefined} />
+          <ModalForm form={"Sign Up"} navToggle={isMobile ? toggle: undefined} />
 
-          <NavBarItem type={'surfer'} list={surfers} />
-          <NavBarItem type={'surf-spot'} list={surfSpotsWithLogEntries} />
-          <NavBarItem type={'country'} list={countriesWithLogEntries} />
+          <NavBarItem 
+            type={'surfer'} 
+            list={surfers}
+            navToggle={isMobile ? toggle: undefined}
+          />
+          <NavBarItem
+            type={'surf-spot'}
+            list={surfSpotsWithLogEntries}
+            navToggle={isMobile ? toggle: undefined}
+          />
+          <NavBarItem
+            type={'country'}
+            list={countriesWithLogEntries}
+            navToggle={isMobile ? toggle: undefined}
+          />
         </Nav>
       </Collapse>
     </Navbar>
