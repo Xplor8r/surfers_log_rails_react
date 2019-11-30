77f6d014109f8ddf648ac30fceac1cc1dd1c20fa
Michael <m.pieschel@iotwarez.com>
2019-11-30 13:52:00 -0800

    import NoLogEntries to LogEntries container, prettier format

--- client/src/Containers/logEntriesContainer.js
+++ client/src/Containers/logEntriesContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Col, Card, CardHeader } from 'reactstrap';
 import LogEntry from '../Components/logEntry';
+import NoLogEntries from '../Components/noLogEntries';
 import { fetchLogEntryData } from '../Actions/logEntries';
 import { fetchLogEntryDataByCountry } from '../Actions/logEntries';
 import { fetchLogEntryDataBySurfSpot } from '../Actions/logEntries';
@@ -35,15 +36,15 @@ class LogEntries extends Component {
             let newId = 0;            
             const stateType = location.state.type
             this.setState({type: stateType});
-            if(stateType !== 'all') { newId = location.state.prop.id }
+            if(stateType !== 'all' && location.state !== undefined) { newId = location.state.prop.id }
             fetch(stateType, newId);
             window.scrollTo(0, 0);
         });
         // on page refresh or intial site load change type, set id and than call fetch
         let propId = 0;
         const propType = this.props.type;
-        this.setState({type: propType});
-        if (propType !== 'all'){ propId = this.props.location.state.prop.id }
+        // this.setState({type: propType});
+        if (propType !== 'all' && this.props.location.state !== undefined){ propId = this.props.location.state.prop.id}
         fetch(propType, propId);
         window.scrollTo(0, 0);
     }
@@ -71,9 +72,12 @@ class LogEntries extends Component {
                         <span>{header}</span>
                     </CardHeader>
                 </Card>
-                {logEntryData.map((logEntry) => (
-                    <LogEntry key={logEntry.id} logEntry={logEntry} />
-                ))}
+                {!logEntryData.hasOwnProperty('error') ? logEntryData.map((logEntry) => {
+                    return (
+                        <LogEntry key={logEntry.id} logEntry={logEntry} />
+                    )
+                }) :
+                <NoLogEntries />}
                 {isMobile && <Ad />}
             </Col>
         )
