a52fc60b30a1f68a61ee081db792d4118481eefc
Michael <m.pieschel@iotwarez.com>
2019-11-30 14:34:16 -0800

    nav dropdown links white, refactor navBarItem component

--- client/src/Components/navBarItem.js
+++ client/src/Components/navBarItem.js
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import { DropdownToggle, DropdownMenu, Dropdown } from 'reactstrap';
-import DynamicLink from './dynamicLink';
+import React, { useState } from "react";
+import { DropdownToggle, DropdownMenu, Dropdown } from "reactstrap";
+import DynamicLink from "./dynamicLink";
 
 // navToggle passed to DynamicLink for when isMobile
 // type passed to DynamicLink
-const NavBarItem = ({list, type, navToggle}) => {
-	// use hooks for dropdown menu toggle
-	const [isOpen, setIsOpen] = useState(false);
-	const toggle = () => setIsOpen(!isOpen);
+const NavBarItem = ({ list, type, navToggle }) => {
+  // use hooks for dropdown menu toggle
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-	return (
-		<Dropdown direction="left" isOpen={isOpen} toggle={toggle} >
-			{type === 'surfer' ?
-				<DropdownToggle nav caret>Surfers</DropdownToggle>:
-				type === 'country' ?
-				<DropdownToggle nav caret>Countries</DropdownToggle>:
-				<DropdownToggle nav caret>Surf Spots</DropdownToggle>
-			}
-				
-			<DropdownMenu
-				modifiers={{
-					setMaxHeight: {
-						enabled: true,
-						order: 890,
-						fn: (data) => {
-							return {
-								...data,
-								styles: {
-									...data.styles,
-									overflow: 'auto',
-									maxHeight: '45vh',
-								},
-							};
-						},
-					},
-				}}
-			>
-				{list.map((item) => (
-					<DynamicLink
-						display={'name'}
-						type={type}
-						prop={item}
-						key={item.id}
-						navToggle={navToggle}
-					/>
-				))}
-			</DropdownMenu>
-		</Dropdown>
-	)
-}
+  return (
+    <Dropdown direction="left" isOpen={isOpen} toggle={toggle}>
+      <DropdownToggle nav caret style={{ color: "white" }}>
+        {type === "surfer"
+          ? "Surfers"
+          : type === "country"
+          ? "Countries"
+          : "Surf Spots"}
+      </DropdownToggle>
+      <DropdownMenu
+        modifiers={{
+          setMaxHeight: {
+            enabled: true,
+            order: 890,
+            fn: data => {
+              return {
+                ...data,
+                styles: {
+                  ...data.styles,
+                  overflow: "auto",
+                  maxHeight: "30vh"
+                }
+              };
+            }
+          }
+        }}
+      >
+        {list.map(item => (
+          <DynamicLink
+            display={"name"}
+            type={type}
+            prop={item}
+            key={item.id}
+            navToggle={navToggle}
+          />
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 
-export default NavBarItem
+export default NavBarItem;
